0d6f02208c1d06a4cf5f5bb93089aeea1bc2f809
Luis Eduardo Betanzos Ibañez <luisbeta670@gmail.com>
2023-03-01 14:16:38 -0600

    lista de hobbies

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  const name = "LUIS EDUARDO BETANZOS IBAÑEZ"
+  return (
+<div>
+  <h3>Lista de hobbies de {name}</h3>
+  <ul>
+    <li>Tocar guitarra</li>
+    <li>jugar videogames</li>
+    <li>leer libros</li>
+  </ul>
+  </div>
+  
+
+
+  )
+
+}
+
+export default App
